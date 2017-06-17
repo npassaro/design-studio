2d8d56891cd2aed77e8026bdd9138dbc2558d987
Nuno Pássaro <nunomcpassaro@gmail.com>
2017-06-17 16:55:55 +0100

    refactor with new IconsWithHiddenLinks component

--- app/javascript/footer/footer.js
+++ app/javascript/footer/footer.js
@@ -2,24 +2,21 @@ import React from 'react';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 import { CircleIcon, TriangleIcon } from '../components/icons';
+import IconWithHiddenLinks from '../components/icon_with_hidden_links';
 
 const Footer = () =>
   <div className="row medium-unstack align-bottom expanded content-footer">
     <div className="columns">
-      <CircleIcon />
-      <ReactCSSTransitionGroup
-        transitionName="iconBox"
-        transitionEnter
-        transitionLeave
-        transitionEnterTimeout={200}
-        transitionLeaveTimeout={200}
-      >
-        <a href="/" className="link--circleBox">Contact & Newsletter</a>
-      </ReactCSSTransitionGroup>
+      <IconWithHiddenLinks
+        icon={<CircleIcon />}
+        links={<a href="/" className="link--circleBox">Contact & Newsletter</a>}
+      />
     </div>
     <div className="columns align-right">
-      <a href="/" className="link--triangleBox">Shop</a>
-      <TriangleIcon />
+      <IconWithHiddenLinks
+        icon={<TriangleIcon />}
+        links={<a href="/" className="link--triangleBox">Shop</a>}
+      />
     </div>
   </div>;
 
